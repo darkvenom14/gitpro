feaa006ba74136831c742f1681a216504e279361
darkvenom14 <blal22550@gamil.com>
2023-01-03 18:44:41 +0530

    check my code

---
+++ file.js
@@ -0,0 +1,18 @@
+    let num= 11 ;
+    let count= 0 ;
+    for( let i= 1 ; i<= num ; i++ ){
+        if ( num% i== 0 ){
+            count++
+        }
+    }
+    ( count== 2 )? console.log( "prime"):
+    console.log("not prime")
+    
+    //plaindrome
+    let str= "naman" ;
+    let rev= "" ;
+    for( let i= str.length-1 ; i>= 0 ; i--){
+        rev+= str[i]
+    }
+    ( str== rev )? console.log( "plaindrome" ):
+    console.log("not plaindrome")
